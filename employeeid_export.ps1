152bc54c88b2ee612181e82a93b332c598ea4437
Nummen <142619567+j3naissante@users.noreply.github.com>
2025-09-19 21:14:52 +0300

    Delete employeeid_export.ps1

--- employeeid_export.ps1
+++
@@ -1,7 +0,0 @@
-
-Get-ADUser -SearchBase 'YOUR OU' -Filter * -Properties employeeID, userPrincipalName, DistinguishedName | 
-
-Select-Object Name, userPrincipalName, employeeID, @{Name="OU";Expression={$_.DistinguishedName -replace '^CN=.*?,OU=(.*?),DC=.*$','$1'}} | 
-
-ogv
-
