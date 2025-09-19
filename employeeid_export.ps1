3249ec79521ba6283dd66e9cab303040a6a128a0
Nummen <142619567+j3naissante@users.noreply.github.com>
2025-09-19 21:11:45 +0300

    Update employeeid_export.ps1

--- employeeid_export.ps1
+++ employeeid_export.ps1
@@ -1,8 +1,7 @@
-# Get all AD users from the specified Organizational Unit (OU)
+
 Get-ADUser -SearchBase 'YOUR OU' -Filter * -Properties employeeID, userPrincipalName, DistinguishedName | 
 
-# Select specific properties and add a custom property for the OU
 Select-Object Name, userPrincipalName, employeeID, @{Name="OU";Expression={$_.DistinguishedName -replace '^CN=.*?,OU=(.*?),DC=.*$','$1'}} | 
 
-# Output the result to an Out-GridView window for easy viewing
 ogv
+
